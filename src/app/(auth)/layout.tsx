3ab272f1787656df874810f893a2e7d226ad4bbb
Hiroki Sugiyama <hiroki.sugiyama.dev@gmail.com>
2025-06-14 19:58:26 +0900

    feat: 認証レイアウトにナビゲーションリンクを追加
    - usePathnameフックを使用して、現在のパスに基づいてボタンのラベルを動的に変更
    - サインインページとサインアップページへのリンクをボタンに追加

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,3 +1,4 @@
+"use client";
 // NOTE: (auth)はRoute Groups（ルートグループ）と呼ばれる機能
 // URLに影響を与えずにルートを整理するためのフォルダ
 // 例: (auth)/sign-in は /sign-in というURLになる（/auth/sign-inではない）
@@ -5,19 +6,27 @@
 
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 interface AuthLayoutProps {
   children: React.ReactNode;
 }
 
 const AuthLayout = ({ children }: AuthLayoutProps) => {
+  const pathname = usePathname();
+  const isSignIn = pathname === "/sign-in";
   return (
     <main className="bg-neutral-100 min-h-screen">
       <div className="mx-auto max-w-screen-2xl p-4">
         <nav className="flex justify-between items-center">
           <Image src="/logo.svg" height={56} width={152} alt="logo" />
           <div className="flex items-center gap-2">
-            <Button variant="secondary">Sign Up</Button>
+            <Button variant="secondary">
+              <Link href={isSignIn ? "/sign-up" : "/sign-in"}>
+                {isSignIn ? "Sign Up" : "Login"}
+              </Link>
+            </Button>
           </div>
         </nav>
         <div className="flex flex-col items-center justify-center pt-4 md:pt-14">
